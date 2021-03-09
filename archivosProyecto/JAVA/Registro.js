e14900f730a9a093b4f889d8409aa7e2957fdaeb
Fabiola <fabiola-2803@hotmail.com>
2021-03-09 00:37:41 -0600

    JS de Pass en Registro, Text Calif curso y alumn en VerCurso

---
+++ archivosProyecto/JAVA/Registro.js
@@ -0,0 +1,29 @@
+function Registrar(form) {
+    var p = document.getElementById("password").value;
+    window.alert(p);
+    var num = false;
+    var carac = false;
+
+    if (p.length >= 8) {
+
+        for (var i = 0; i < p.length; i++) {
+            var ch = p.charAt(i); 
+            window.alert(ch);
+            if (ch >= "0" || ch <= "9")
+                num = true;
+            if (ch == "/" || ch == "*" || ch == "¡" || ch == "”" || ch == "#" || ch == "$" || ch == "%" || ch == "&" || ch == "=" || ch == "’" || ch == "?" || ch == "¡" || ch == "¿" || ch == ":" || ch == ";" || ch == "," || ch == "."
+                || ch == "-" || ch == "_" || ch == "+" || ch == "{" || ch == "]" || ch == "[" || ch == "}") {
+                carac = true;
+            }
+
+        }
+        if (num == true && carac == true)
+        window.alert("Contraseña guardada");
+        else{
+        window.alert("La contraseña debe tener al menos un numero y un caracter especial")
+        }
+    }
+    else
+        window.alert("La contraseña debe tener 8 caracteres")
+   
+}
