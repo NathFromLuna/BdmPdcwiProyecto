4745325cf8d832098dc138e0a5bac773c010ca85
NathFromLuna <natalysc366@gmail.com>
2021-03-09 19:28:53 -0600

    aun no direcciona

--- archivosProyecto/javas/llevameA.js
+++ archivosProyecto/javas/llevameA.js
@@ -1,12 +1,13 @@
-function llevameAInSes(){
-    //window.open("/archivosProyecto/Html's/IS.html",false);
-    window.location.href = "archivosProyecto/Html's/IS.html";
+function llevameAInSes() {
+    //window.open("/archivosProyecto/Html's/IS.html",_self);
+    window.location.href = "/archivosProyecto/Html's/.html";
+   
 }
-function llevamePerfil(){
+function llevamePerfil() {
     //window.open("/archivosProyecto/Html's/perfil.html",false);
     window.location.href = "archivosProyecto/Html's/perfil.html";
 }
-function llevameRegistrarme(){
+function llevameRegistrarme() {
     //window.open("archivosProyecto/Html's/Registro.htm","self");
     window.location.href = "archivosProyecto/Html's/Registro.htm";
 }
