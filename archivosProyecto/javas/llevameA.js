b11b71d84cec6ea9c1207787f5d06da2302ffc0f
NathFromLuna <natalysc366@gmail.com>
2021-03-09 19:17:30 -0600

    no logro redireccionar

---
+++ archivosProyecto/javas/llevameA.js
@@ -0,0 +1,12 @@
+function llevameAInSes(){
+    //window.open("/archivosProyecto/Html's/IS.html",false);
+    window.location.href = "archivosProyecto/Html's/IS.html";
+}
+function llevamePerfil(){
+    //window.open("/archivosProyecto/Html's/perfil.html",false);
+    window.location.href = "archivosProyecto/Html's/perfil.html";
+}
+function llevameRegistrarme(){
+    //window.open("archivosProyecto/Html's/Registro.htm","self");
+    window.location.href = "archivosProyecto/Html's/Registro.htm";
+}
