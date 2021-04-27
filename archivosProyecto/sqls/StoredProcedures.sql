8e34c221f4aa90dc84ec677c3526ea4d331ca896
NathFromLuna <natalysc366@gmail.com>
2021-04-26 22:54:42 -0500

    more sqls

--- archivosProyecto/sqls/StoredProcedures.sql
+++ archivosProyecto/sqls/StoredProcedures.sql
@@ -17,7 +17,78 @@ begin
     where id=Usid_usuario and esMaestro=true;
 end/
     
+delimiter /
+create procedure registrarUsuario (in nNombre varchar(50),
+ in nApellidos varchar(150),
+ in nNickname varchar(100),
+ in nCorreo varchar(70),
+ in nContraseña varbinary(40),
+ in esTeacher bool,
+ in nImagenPerfil blob)
+begin
+    insert into Usuarios(nombre, apellidos, nickname, correo, contraseña,
+    esMaestro, imagenPerfil)
+    values(nNombre, nApellidos, nNickname, nCorreo, nContraseña,
+    esTeacher, nImagenPerfil);
+end/
+
+delimiter /
+create procedure registrarCurso (in nNombre varchar(70),
+ in nDescripcion varchar(200),
+ in nImagenCurso blob,
+ in nVideoTrailer blob,
+ in nCosto float,
+ in nCantidadNivelesCurso int,
+ in nId_profesor int)
+begin
+    insert into Curso(nombre, descripcion, imagenCurso, videoTrailer, 
+    costo, cantidadNivelesCurso, id_profesor)
+    values(nNombre, nDescripcion, nImagenCurso, nVideoTrailer, 
+    nCosto, nCantidadNivelesCurso, nId_profesor);
+end/
+
+delimiter /
+create procedure registrarNivel (in nId_curso int,
+ in nVideoLvl blob,
+ in nOtrosArchivo blob,
+ in nNumeroNivel int)
+begin
+    insert into Niveles(id_curso, videoLvl, otrosArchivo, numeroNivel)
+    values(nId_curso, nVideoLvl, nOtrosArchivo, nNumeroNivel);
+end/
+
+delimiter /
+create procedure editarUsuario (in idUs int, in nNombre varchar(50),
+ in nApellidos varchar(150),
+ in nNickname varchar(100),
+ in nCorreo varchar(70),
+ in nContraseña varbinary(40),
+ in esTeacher bool,
+ in nImagenPerfil blob)
+begin
+
+
+    insert into Usuarios(nombre, apellidos, nickname, correo, contraseña,
+    esMaestro, imagenPerfil)
+    values(nNombre, nApellidos, nNickname, nCorreo, nContraseña,
+    esTeacher, nImagenPerfil);
+end/
+
+
+delimiter /
+create procedure eliminarUsuario (in _usID int)
+begin
+    delete from usuarios where usID=_usID;
+end/
+
   
+delimiter /
+create procedure buscarCurso (in cursoAbuscar varchar(200))
+begin
+    select nombre, id_curso from Curso where nombre like cursoAbuscar ;  -- concat(%, _NombreUsuario, %)
+end/
+
+call buscarCurso ('%r%') -- enviar comilla, porcentaje, variable, porcentaje, comilla
     
     
     
