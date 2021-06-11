924c466f51fdfa74c70760e2c209ed769a7db147
NathFromLuna <natalysc366@gmail.com>
2021-06-10 19:21:28 -0500

    cambio

--- archivosProyecto/Html's/profilePicture.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    $variablevarialosa =  $_SESSION["imagenP"];
-    header("content_type: image/jpeg");
-    echo  $variablevarialosa;
-    ?>
--- archivosProyecto/sqls/Triggers.sql
+++
@@ -1,8 +0,0 @@
-delimiter $$
-CREATE trigger CategoriaParaCurso 
-after insert on Curso for each row 
-BEGIN
-	INSERT INTO tablaAsociativaCursoCategoria ( id_cat, id_curso)
-    values (, new.id_curso)
-END $$
-
